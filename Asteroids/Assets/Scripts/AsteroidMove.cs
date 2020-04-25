2c6f834fdcad35a2c50b680bee0127868f65c939
Jeremy Rylicki <jeremy.rylicki@ndus.edu>
2020-04-25 13:20:15 -0500

    Added Asteroid Spawns

---
+++ Asteroids/Assets/Scripts/AsteroidMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidMove : MonoBehaviour
+{
+    public float minSpeed = 5f;
+    public float maxSpeed = 15f;
+    private Rigidbody2D rb;
+    private Camera MainCamera;
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float xSpeed = Random.Range(minSpeed, maxSpeed);
+        float ySpeed = Random.Range(minSpeed, maxSpeed);
+        if(transform.position.x > 0)
+        {
+            xSpeed = xSpeed * -1;
+        }
+        if(transform.position.y > 0)
+        {
+            ySpeed = ySpeed * -1;
+        }
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(xSpeed, ySpeed);
+        MainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        remove();
+    }
+
+    private void remove()
+    {
+        if ((transform.position.x < screenBounds.x * -1 - 10) ||
+            (transform.position.x > screenBounds.x + 10) || (transform.position.y < screenBounds.y * -1 - 10) ||
+            (transform.position.y > screenBounds.y + 10))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Asteroids/Assets/Scripts/SpawnAsteroid.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAsteroid : MonoBehaviour
+{
+    public Camera MainCamera;
+    public GameObject asteroid;
+    public float spawnDelay = 10f;
+    private float nextSpawnTime;
+    private Vector2 screenBounds;
+
+    void Start()
+    {
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+    }
+
+    void Update()
+    {
+        if (shouldSpawn())
+        {
+            spawnLoaction();
+            spawn();
+        }
+    }
+
+    private bool shouldSpawn()
+    {
+        return Time.time >= nextSpawnTime;
+    }
+    private void spawnLoaction()
+    {
+        Vector3 newLocation = new Vector3(0,0,0);
+        float side = Random.Range(0f, 4f);
+        if(side <= 1)
+        {
+            newLocation.y = screenBounds.y + 5;
+            newLocation.x = Random.Range(-screenBounds.x, screenBounds.x);
+
+        }
+        else if (side > 1 && side <= 2)
+        {
+            newLocation.y = -screenBounds.y - 5 ;
+            newLocation.x = Random.Range(-screenBounds.x, screenBounds.x);
+
+        }
+        else if (side > 2 && side <= 3)
+        {
+            newLocation.x = screenBounds.x + 5 ;
+            newLocation.y = Random.Range(-screenBounds.y, screenBounds.y);
+
+        }
+        else if (side > 3 && side <= 4)
+        {
+            newLocation.x = -screenBounds.x - 5;
+            newLocation.y = Random.Range(-screenBounds.y, screenBounds.y);
+            
+        }
+        transform.position = newLocation;
+    }
+    private void spawn()
+    {
+        nextSpawnTime = Time.time + spawnDelay;
+        Instantiate(asteroid, transform.position, transform.rotation);
+    } 
+
+
+    
+}
